5502ec0a139819c42e6b49f8168ac70aa14eefdf
tejaKoribilli <123244593+tejaKoribilli@users.noreply.github.com>
2023-05-20 18:24:20 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,8 @@
 // Write your code here
+import {Component} from 'react'
+import React from 'react'
+import Popup from 'reactjs-popup'
+
 import {Link} from 'react-router-dom'
 import {GiHamburgerMenu} from 'react-icons/gi'
 
@@ -6,19 +10,58 @@ import 'reactjs-popup/dist/index.css'
 
 import './index.css'
 
-const Header = () => (
-  <div className="header-container">
-    <Link to="/">
-      <img
-        className="website-logo"
-        src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
-        alt="website logo"
-      />
-    </Link>
-    <button type="button" className="menu-btn">
-      <GiHamburgerMenu className="menu-icon" />
-    </button>
-  </div>
-)
+class Header extends Component {
+  state = {showPopUp: false}
+
+  onClickMenu = () => {
+    this.setState(prevState => {
+      showPopUp: !prevState.showPopUp
+    })
+  }
+
+  renderPopUp = () => (
+    <>
+      <Popup trigger={<button>Open Popup</button>} modal>
+        {close => (
+          <div className="popup">
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+            <div className="content">
+              <h2>Popup Content</h2>
+              <p>This is the content of the popup.</p>
+            </div>
+          </div>
+        )}
+      </Popup>
+    </>
+  )
+
+  renderHeader = () => (
+    <>
+      <Link to="/">
+        <img
+          className="website-logo"
+          src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
+          alt="website logo"
+        />
+      </Link>
+      <button type="button" className="menu-btn" onClick={this.onClickMenu}>
+        <GiHamburgerMenu className="menu-icon" />
+      </button>
+    </>
+  )
+
+  render() {
+    const {showPopUp} = this.state
+
+    return (
+      <div className="header-container">
+        {!showPopUp && this.renderHeader()}
+        {showPopUp && this.renderPopUp()}
+      </div>
+    )
+  }
+}
 
 export default Header
